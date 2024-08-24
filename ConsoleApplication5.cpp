7bd176965c6b79d8494b9666f94a1bb72ef249f9
stubborngenius <dayd3207@gmail.com>
2024-08-24 11:40:56 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int ans = 0;
+void jump(int n, int pos) {
+	if (pos > n) {
+		return;
+	}
+	else if (pos == n) {
+		ans++;
+		return;
+	}
+	else {
+		jump(n, pos + 1);
+		jump(n, pos + 2);
+		jump(n, pos + 3);
+	}
+}
+int main() {
+	int n, pos;
+	cin >> n;
+	pos = 0;
+	jump(n, pos);
+	cout << ans;
+	return 0;
+}
